293bb0532f503fd32cde4707c19cfed08a9a248b
Akanksha Shukla <63357474+sakanksha19@users.noreply.github.com>
2022-02-11 09:18:14 +0530

    Add files via upload

---
+++ src/Sdata.jsx
@@ -0,0 +1,33 @@
+const Sdata = [
+    {
+         sname:"MAID", 
+         imgsrc:"https://wallpapercave.com/wp/wp10314026.jpg",
+         title:"A Netflix Orignal Series",
+         link:"https://www.netflix.com/in/title/81166770"
+    },
+    { 
+         sname:"SQUID GAME" ,
+         imgsrc:"https://wallpapercave.com/dwp1x/wp9932803.png",
+         title:"A Netflix Orignal Series",
+         link:"https://www.netflix.com/in/Title/81040344"
+    },
+    { 
+         sname:"SEX EDUCATION" ,
+         imgsrc:"https://wallpapercave.com/dwp1x/wp5152695.jpg",
+         title:"A Netflix Orignal Series",
+         link:"https://www.netflix.com/in/title/80197526"
+    },
+    { 
+         sname:"THE CHAIR",
+         imgsrc:"https://m.media-amazon.com/images/M/MV5BZmZjNzU3NzEtOTQ2Yi00ZTg2LTg2NmUtMTdlZWUwMWY4M2U5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+         title:"A Netflix Orignal Series",
+         link:"https://www.netflix.com/in/title/81206259"
+    },
+    { 
+         sname:"I THINK YOU SHOULD LEAVE WITH TIM ROBINSON",
+         imgsrc:"https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f65c2979804989.5ccee1b3f2293.jpg",
+         title:"A Netflix Orignal Series",
+         link:"https://www.netflix.com/in/title/80986854"
+    }
+]
+export default Sdata;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from "./Cards";
+import "./index.css";
+import Sdata from "./Sdata";
+
+ReactDOM.render(
+    <>
+      <h1 className="heading_style">List of top 5 Netflix Series in 2021</h1>
+      {Sdata.map((val)=>{
+         return(
+            <Card
+            imgsrc={val.imgsrc}
+            title={val.title}
+            sname={val.sname}
+            link={val.link}
+            />
+         );
+      })}
+    </>,  
+document.getElementById('root')
+)
